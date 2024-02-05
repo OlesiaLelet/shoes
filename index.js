aa15272441e835516f2b6b9edfbedb1d7805798f
Olesia <leletolesa@gmail.com>
2024-02-05 11:13:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// function addListForBurger() {
+//   const showNav = document.getElementById("show-burger");
+//   const showedNav = document.getElementById("showed-m");
+  
+//   showNav.addEventListener("click", function() {
+//     console.log(" Hello");
+//    showedNav.classList.add("burger-menu--show");
+//    })
+// }
