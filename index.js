52d894ce6b2e8b7426e735ffd05366e6082ab73c
Olesia <leletolesa@gmail.com>
2024-02-14 20:09:23 +0200

    for check

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// function addListForBurger() {
-//   const showNav = document.getElementById("show-burger");
-//   const showedNav = document.getElementById("showed-m");
+ function addListForBurger() {
+   const showNav = document.getElementById("show-burger");
+   const hide=document.getElementById("hide-burger")
+   const showedNav = document.getElementById("showed-m");
   
-//   showNav.addEventListener("click", function() {
-//     console.log(" Hello");
-//    showedNav.classList.add("burger-menu--show");
-//    })
-// }
+   showNav.addEventListener("click", function() {
+     console.log(" Hello");
+    showedNav.classList.add("burger-menu--show");
+    })
+    hide.addEventListener("click", function() {
+    showedNav.remove("burger-menu--show");
+
+    })
+ }
