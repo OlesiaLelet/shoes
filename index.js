16b7d90cdadd0542079d593b2b36ef7e1ebf9aed
Olesia <leletolesa@gmail.com>
2024-02-25 22:09:57 +0200

    fixed_JS

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
- function addListForBurger() {
-   const showNav = document.getElementById("show-burger");
-   const hide=document.getElementById("hide-burger")
-   const showedNav = document.getElementById("showed-m");
-  
-   showNav.addEventListener("click", function() {
-     console.log(" Hello");
-    showedNav.classList.add("burger-menu--show");
-    })
-    hide.addEventListener("click", function() {
-    showedNav.remove("burger-menu--show");
 
-    })  
- }
- addListForBurger() 
+
+function addListForBurger() {
+  const showNav = document.getElementById("show-burger");
+  const hide = document.getElementById("hide-burger");
+  const showedNav = document.getElementById("showed-m");
+
+  showNav.addEventListener("click", function () {
+    showedNav.classList.add("burger-menu--show");
+  });
+  hide.addEventListener("click", function () {
+    showedNav.classList.remove("burger-menu--show");
+  });
+}
+
+addListForBurger();
